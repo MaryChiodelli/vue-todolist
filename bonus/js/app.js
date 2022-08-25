0ee33e863f35e89393d0ae22eddda047e3ef3441
MaryChiodelli <marychiodelli97@gmail.com>
2022-08-25 16:40:48 +0200

    add enter functionality

---
+++ bonus/js/app.js
@@ -0,0 +1,49 @@
+const todos = [
+    {
+        text: 'Fare la spesa',
+        done: false
+    },
+    {
+        text: 'Prendere il giornale',
+        done: false
+    },
+    {
+        text: 'Preparare il pranzo',
+        done: true
+    },
+    {
+        text: 'Tagliare il prato',
+        done: false
+    }
+];
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        todos: todos,
+        newTodo: ''
+    },
+    methods: {
+        toggleDone(todo) {
+            todo.done = !todo.done;
+        },
+        addTodo() {
+            this.newTodo = this.newTodo.trim();
+
+            if (!this.newTodo) {
+                return;
+            }
+
+            this.todos.push(
+                {
+                    text: this.newTodo,
+                    done: false
+                }
+            );
+            
+            this.newTodo = '';
+        }
+    }
+});
+
+console.log(app);
